ed6f9d4cb037a6a38426a5da6404c3986b500326
gauraansh <113711977+gauraaansh@users.noreply.github.com>
2022-10-12 06:28:15 -0400

    Update pink_detection.py

--- pink_detection.py
+++ pink_detection.py
@@ -5,6 +5,6 @@ img = cv2.imread('/home/ubuntu/Desktop/grid.png')
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 mask = cv2.inRange(hsv,(85, 100, 20), (95, 255, 255) )
 
-x, y, z = np.where(mask==(255,255,255))
+x, y, z = np.where(img==(255,255,255))
 points =zip(x,y)
 print(points)
